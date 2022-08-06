71ebcabdd9cb3a54d1c58f51b1a766b3ee57fa15
pranab56 <pronabhalder53@gmail.com>
2022-08-07 02:14:09 +0600

    google sign in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 
+import { getAuth } from 'firebase/auth';
 import { useEffect, useState } from 'react';
-import { Routes,Route } from 'react-router-dom';
+import { Routes,Route, useNavigate } from 'react-router-dom';
+
 import './App.css';
+import { app } from './component/Firebase.init';
+
 
 
 
@@ -10,18 +14,30 @@ import Home from './component/Home';
 import Login from './component/Login';
 
 function App() {
-  const firebaseAuth=auth
-  const {auth,setAuth}=useState(false||window.localStorage.getItem("auth")==="true")
-  useEffect(()=>{
-    firebaseAuth.onAuthStateChanged((userCred)=>{
-      console.log(userCred);
-    })
-  },[])
+  const navigate=useNavigate()
+  const firebaseAuth=getAuth(app);
+
+  const [auth,setAuth]=useState(false||window.localStorage.getItem("auth")==="true");
+
+useEffect(()=>{
+  firebaseAuth.onAuthStateChanged((userCard)=>{
+    if(userCard){
+      userCard.getIdToken().then((token)=>{
+        console.log(token);
+      })
+    }
+    else{
+      setAuth(false);
+      window.localStorage.setItem("auth","false");
+      navigate('/login')
+    }
+  })
+},[])
   return (
     <div className="w-screen h-screen bg-blue-500 justify-center items-center">
       <Routes>
         <Route path='/' element={<Home> </Home>}></Route>
-        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/login' element={<Login setAuth={setAuth}></Login>}></Route>
       </Routes>
     </div>
   );
--- src/component/Login.js
+++ src/component/Login.js
@@ -1,32 +1,60 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { FcGoogle } from "react-icons/fc";
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
-import auth from "./Firebase.init";
-import Loading from "../page/Loading";
+import { app } from "./Firebase.init";
+import { getAuth } from "firebase/auth";
+
+import { useSignInWithGoogle } from "react-firebase-hooks/auth";
 import { useNavigate } from "react-router-dom";
+import Loading from "react-loading";
+
+const Login = ({ setAuth }) => {
+  const navigate = useNavigate();
+
+  const firebaseAuth = getAuth(app);
+  const [signInWithGoogle, user, loading, error] =
+    useSignInWithGoogle(firebaseAuth);
+
+    useEffect(()=>{
+      if(window.localStorage.getItem("auth")==="true"){
+        navigate("/",{replace:true})
+      }
+    },[]);
 
 
 
-
-const Login = () => {
-  const navigate=useNavigate()
-  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
-if(user){
-  
-}
 if(loading){
   return <Loading></Loading>
 }
 
 
+  if (user) {
+    setAuth(true);
+    window.localStorage.setItem("auth", "true");
+    firebaseAuth.onAuthStateChanged((userCard) => {
+      if (userCard) {
+        userCard.getIdToken().then((token)=>{
+          console.log(token);
+         
+        })
+        navigate("/", { replace: true });
+      } else {
+        setAuth(false);
+        navigate("/login");
+      }
+    });
+  };
+
+ 
+
   return (
     <div className="relative w-screen h-screen">
       <div className="absolute inset-0 bg-gray-500 flex justify-center items-center p-4">
         <div className="w-full md:w-[375px] p4 bg-gray-200 shadow-2xl rounded-md backdrop-blur-md p-4 flex flex-col items-center justify-center">
-          <div className="flex items-center justify-center gap-2 px-4 py-2 rounded-md bg-gray-300 cursor-pointer hover:bg-white hover:shadow-md duration-100 ease-in-out transition-all"
-          onClick={()=>signInWithGoogle()}
+          <div
+            className="flex items-center justify-center gap-2 px-4 py-2 rounded-md bg-gray-300 cursor-pointer hover:bg-white hover:shadow-md duration-100 ease-in-out transition-all"
+            onClick={() => signInWithGoogle()}
           >
-            <FcGoogle className="text-xl"/>
+            <FcGoogle className="text-xl" />
             Sign in with Google
           </div>
         </div>
